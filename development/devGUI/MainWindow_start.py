8a2d1cd188c4b220a8591875e5ed754c7fb3b1ec
IlyazRA <ilyazraukhillahadzaqi@mail.ugm.ac.id>
2024-11-10 21:50:35 +0700

    anjir lah

--- development/devGUI/MainWindow_start.py
+++ development/devGUI/MainWindow_start.py
@@ -9,3 +9,25 @@ class MainWindow:
         self.main_win = QMainWindow()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_win)
+
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pg_home)
+        self.ui.btn_default.clicked.connect(self.show_default)
+        self.ui.btn_custom.clicked.connect(self.show_custom)
+        self.ui.btn_default_take.clicked.connect(self.show_default_algo)
+
+
+    def show(self):
+        self.main_win.show()
+
+    def show_default(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pg_default)
+    def show_custom(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pg_custom)
+    def show_default_algo(self):
+        self.ui.stackedWidget_2.setCurrentWidget(self.ui.pg_default_algo)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main_win = MainWindow()
+    main_win.show()
+    sys.exit(app.exec_())
