d698e8388b5f147e9811fe10abde7f0d4f3e68a6
theunra <diandrarizqy7@gmail.com>
2024-12-09 17:28:45 +0700

    ok

--- development/models/custom/svm_ext/svm.py
+++
@@ -1,6 +0,0 @@
-from development.devGUI.classifier import BaseClassifier
-
-class Classifier(BaseClassifier):
-    def predict(self, datas):
-        print("Custom SVM EXT Predictor")
-        return [0, 1]
